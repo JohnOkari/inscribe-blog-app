3cb7b1cee07c40d25dc0d7588f0d2b7c9bf12d8f
JohnOkari <johnnokari@gmail.com>
2023-03-17 00:37:59 +0300

    edit create

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,30 +1,38 @@
 class BlogsController < ApplicationController
+
+  # get blogs
     def index
         blogs = Blog.all
         render json: blogs
       end
 
+  # show blog
       def show
         blog = Blog.find_by(id: params[:id])
         render json: blog
       end
-    
+
+  # create blog
       def create
         blog = Blog.create(blog_params)
         render json: blog, status: :created
       end
     
+  # update blog
       def update
         blog = Blog.find_by(id: params[:id])
         blog.update(blog_params)
         render json: blog, status: :created
       end
     
+  # delete blog
       def destroy
         blog = Blog.find_by(id: params[:id])
         blog.destroy
         head :no_content
       end
+
+
       private
       
       def blog_params
